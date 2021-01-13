0dd3aa4ba9d2dbd02d29cc38032a2de877335987
Abhishekh Sivakumar <abhishekh2001@gmail.com>
2021-01-13 22:43:31 +0530

    Init, frontend

---
+++ frontend/services/authService.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+const baseUrl = '/api/auth'
+
+// TODO: watch travesty for proxy
+
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button'
+
+const Home = () => {
+    return (
+        <div>
+            <Button variant="contained" color="secondary">
+                Hello World
+            </Button>
+        </div>
+    )
+}
+
+export default Home
