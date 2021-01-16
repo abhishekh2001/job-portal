3f7ea8c75e7aa59f6a63af418d257fed987265e4
ABHISHEKH SIVAKUMAR <abhishekh.sivakumar@students.iiit.ac.in>
2021-01-16 13:27:50 +0530

    AppToolBar

--- frontend/src/components/AppToolBar.js
+++ frontend/src/components/AppToolBar.js
@@ -3,25 +3,62 @@ import {
     Toolbar, Typography
 } from '@material-ui/core'
 import clsx from 'clsx'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import {mainListItems, secondaryListItems} from './listitems'
+import Drawer from '@material-ui/core/Drawer'
+import React from 'react'
 
+const drawerWidth = 240
 
 const AppToolBar = ({classes}) => {
+    const [open, setOpen] = React.useState(true)
+    const handleDrawerOpen = () => {
+        setOpen(true)
+    }
+    const handleDrawerClose = () => {
+        setOpen(false)
+    }
+
     return (
-        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-            <Toolbar className={classes.toolbar}>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                    Dashboard
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div>
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        Dashboard
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon/>
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>{mainListItems}</List>
+                <Divider/>
+                <List>{secondaryListItems}</List>
+            </Drawer>
+        </div>
     )
 }
+
+export default AppToolBar
--- frontend/src/components/routes/Admin.js
+++ frontend/src/components/routes/Admin.js
@@ -2,10 +2,95 @@ import {useAuth} from '../../context/auth'
 import Button from '@material-ui/core/Button'
 import {Redirect} from 'react-router-dom'
 import {useState} from 'react'
+import AppToolBar from '../AppToolBar'
+import {makeStyles} from '@material-ui/core'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
 
 function Admin() {
     const [loggedIn, setLoggedIn] = useState(true)
     const { setAuthTokens } = useAuth()
+
+    const classes = useStyles()
 
     function logOut() {
         setAuthTokens(null)
@@ -19,6 +104,7 @@ function Admin() {
 
     return (
         <div>
+            <AppToolBar classes={classes} />
             <div>Admin Page</div>
             <Button size="large" color="secondary" onClick={logOut}>Log out</Button>
         </div>
