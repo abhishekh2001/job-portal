c6155e77acf7ea92e511a69cd1e4e6d65cc033f6
ABHISHEKH SIVAKUMAR <abhishekh.sivakumar@students.iiit.ac.in>
2021-01-23 20:06:17 +0530

    changes to sorting

--- frontend/src/components/CustomTable.js
+++ frontend/src/components/CustomTable.js
@@ -21,26 +21,33 @@ export default function CustomTable(records, headCells, filterFn) {
         const stabilizedThis = array.map((el, index) => [el, index])
         stabilizedThis.sort((a, b) => {
             const order = comparator(a[0], b[0])
-            if (order !== 0) return order
-            return a[1] - b[1]
+            // if (order) return order
+            // return a[1] - b[1]
+            return order ? order : a[1] - b[1]
         })
         return stabilizedThis.map((el) => el[0])
     }
 
     function getComparator(order, orderBy) {
-        return order === 'desc'
-            ? (a, b) => descendingComparator(a, b, orderBy)
-            : (a, b) => -descendingComparator(a, b, orderBy)
-    }
 
-    function descendingComparator(a, b, orderBy) {
-        if (b[orderBy] < a[orderBy]) {
-            return -1
+        console.log('here')
+        if (order === 'desc') {
+            return (a, b) => {
+                if (b[orderBy] < a[orderBy])
+                    return -1
+                if (b[orderBy] > a[orderBy])
+                    return 1
+                return 0
+            }
+        } else {
+            return (a, b) => {
+                if (b[orderBy] < a[orderBy])
+                    return 1
+                if (b[orderBy] > a[orderBy])
+                    return -1
+                return 0
+            }
         }
-        if (b[orderBy] > a[orderBy]) {
-            return 1
-        }
-        return 0
     }
 
     const recordsAfterSorting = () => {
